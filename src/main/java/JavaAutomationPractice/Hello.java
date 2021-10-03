8731d7d621c498d7e4671cc630d99fbec0c87de7
PiratesX007 <anubhav.techdalela@gmail.com>
2021-10-03 13:29:14 +0530

    adding code for existing file

--- src/main/java/JavaAutomationPractice/Hello.java
+++ src/main/java/JavaAutomationPractice/Hello.java
@@ -4,5 +4,10 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hi this isn java class");
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i);
+
+        }
     }
 }
